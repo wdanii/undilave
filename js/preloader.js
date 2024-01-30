6168eecd9b798a3e48857dff6b23fe4601f8b3ae
Daniel Solano <53199385+wdanii@users.noreply.github.com>
2024-01-30 14:16:16 -0500

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("preloader").style.display = "none";
+        document.body.style.display = "block";
+    }, 1800);
+});
